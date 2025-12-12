3e411e2be9a2aa144b791993dc0f105a7e81ed86
System12e34 <system12e34@gmail.com>
2025-12-12 23:24:24 +0200

    Changed logig

--- days/day04/4-1.py
+++ days/day04/4-1.py
@@ -11,16 +11,42 @@ with open(INPUT_PATH) as file:
         input_list.append(line.strip())
 
 
-number_of_positions:int = 8
+
 number_elements:int = len(input_list[0])
-index:int = 0
+rolls_symbol:str = "@"
+total_of_rolls_symbol:int = 0
 
-matrix2 = np.array([list(row) for row in input_list])
+matrix = np.array([list(row) for row in input_list])
 
 
-for raw_index, raw_list in enumerate(matrix2):
+for raw_index, raw_list in enumerate(matrix):
     print(raw_index, raw_list)
     for val_index, list_element in enumerate(raw_list):
-        if list_element == "@":
-            pass
+        print(val_index, list_element)
+        if list_element == rolls_symbol:
+            if val_index > 0:
+                left_element = matrix[raw_index][val_index-1]
+            if val_index < number_elements-1:
+                right_element = matrix[raw_index][val_index+1]
+                print(right_element)
+
+
+
+
+
+
+
+        # if list_element == rolls_symbol:
+        #     left_rng_for_check = raw_list[0: val_index]
+        #     left_count_of_rolls_symbol = (np.count_nonzero(left_rng_for_check[-8:] == rolls_symbol))
+        #     right_rng_for_check = raw_list[val_index+1:]
+        #     right_count_of_rolls_symbol = (np.count_nonzero(right_rng_for_check[:8] == rolls_symbol))
+        #     print(right_count_of_rolls_symbol)
+
+
+            # if left_count_of_rolls_symbol <5: #and right_count_of_rolls_symbol <5:
+            #     total_of_rolls_symbol +=1
+
+            #print(total_of_rolls_symbol)
+
 
