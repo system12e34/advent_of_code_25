af2f3839d5fd11cc0c982ce48884a60dd68813c6
System12e34 <system12e34@gmail.com>
2025-12-14 22:52:58 +0200

    Solved 4-1 day

--- days/day04/4-1.py
+++ days/day04/4-1.py
@@ -11,24 +11,57 @@ with open(INPUT_PATH) as file:
         input_list.append(line.strip())
 
 
-
-number_elements:int = len(input_list[0])
+number_cols:int = len(input_list[0])
+number_raws:int = len(input_list)
 rolls_symbol:str = "@"
 total_of_rolls_symbol:int = 0
-
+number_rolls_for_element:int = 0
 matrix = np.array([list(row) for row in input_list])
 
 
 for raw_index, raw_list in enumerate(matrix):
-    print(raw_index, raw_list)
     for val_index, list_element in enumerate(raw_list):
-        print(val_index, list_element)
+        number_rolls_for_element = 0
         if list_element == rolls_symbol:
             if val_index > 0:
                 left_element = matrix[raw_index][val_index-1]
-            if val_index < number_elements-1:
+                if left_element == rolls_symbol:
+                    number_rolls_for_element += 1
+            if val_index < number_cols-1:
                 right_element = matrix[raw_index][val_index+1]
-                print(right_element)
+                if right_element == rolls_symbol:
+                    number_rolls_for_element += 1
+            if raw_index > 0:
+                upper_element = matrix[raw_index-1][val_index]
+                if upper_element == rolls_symbol:
+                    number_rolls_for_element += 1
+            if raw_index < number_raws-1:
+                lower_element = matrix[raw_index+1][val_index]
+                if lower_element == rolls_symbol:
+                    number_rolls_for_element += 1
+            if raw_index > 0 and val_index > 0:
+                upper_left_element = matrix[raw_index-1][val_index-1]
+                if upper_left_element == rolls_symbol:
+                    number_rolls_for_element += 1
+            if raw_index > 0 and val_index < number_cols-1:
+                upper_right_element = matrix[raw_index-1][val_index+1]
+                if upper_right_element == rolls_symbol:
+                    number_rolls_for_element += 1
+            if raw_index < number_raws-1 and val_index > 0:
+                lower_left_element = matrix[raw_index+1][val_index-1]
+                if lower_left_element == rolls_symbol:
+                    number_rolls_for_element += 1
+            if raw_index < number_raws-1 and val_index < number_cols-1:
+                lower_right_element = matrix[raw_index+1][val_index+1]
+                if lower_right_element == rolls_symbol:
+                    number_rolls_for_element += 1
+            if number_rolls_for_element < 4:
+                total_of_rolls_symbol += 1
+
+print(total_of_rolls_symbol)
+
+
+
 
 
 
